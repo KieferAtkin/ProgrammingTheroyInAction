12dfc2a03ff0c155e9f17152f40ce6dbab5062d9
KieferAtkin <54629417+KieferAtkin@users.noreply.github.com>
2022-07-28 16:46:54 +0100

    Aim and Normal Sensitivity

--- ThirdPersonController/Assets/ThirdPersonShooter/ThirdPersonShooterController.cs
+++ ThirdPersonController/Assets/ThirdPersonShooter/ThirdPersonShooterController.cs
@@ -6,13 +6,16 @@ using StarterAssets;
 
 public class ThirdPersonShooterController : MonoBehaviour
 {
-    [SerializeField] 
-    private CinemachineVirtualCamera aimVirtualCamera;
+    [SerializeField] private CinemachineVirtualCamera aimVirtualCamera;
+    [SerializeField] private float normalSensitivity;
+    [SerializeField] private float aimSensitivity;
 
+    private ThirdPersonController thirdPersonController;
     private StarterAssetsInputs starterAssetsInputs;
 
     private void Awake()
     {
+        thirdPersonController = GetComponent<ThirdPersonController>();
         starterAssetsInputs = GetComponent<StarterAssetsInputs>();
     }
 
@@ -21,10 +24,12 @@ public class ThirdPersonShooterController : MonoBehaviour
          if (starterAssetsInputs.aim)
         {
             aimVirtualCamera.gameObject.SetActive(true);
+            thirdPersonController.SetSensitivity(aimSensitivity);
         }
         else
         {
             aimVirtualCamera.gameObject.SetActive(false);
+            thirdPersonController.SetSensitivity(normalSensitivity);
         }
     }
 }
