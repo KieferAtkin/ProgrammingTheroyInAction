2819623a412b79580671d319423cdbff5a50e5ce
KieferAtkin <kieferatkin91@gmail.com>
2022-07-29 17:03:02 +0100

    AimingDebug1

--- ThirdPersonController/Assets/ThirdPersonShooter/ThirdPersonShooterController.cs
+++ ThirdPersonController/Assets/ThirdPersonShooter/ThirdPersonShooterController.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using Cinemachine;
 using StarterAssets;
@@ -9,6 +7,9 @@ public class ThirdPersonShooterController : MonoBehaviour
     [SerializeField] private CinemachineVirtualCamera aimVirtualCamera;
     [SerializeField] private float normalSensitivity;
     [SerializeField] private float aimSensitivity;
+    [SerializeField] private LayerMask aimColliderLayerMask = new LayerMask();
+    [SerializeField] private Transform debugTransform;
+
 
     private ThirdPersonController thirdPersonController;
     private StarterAssetsInputs starterAssetsInputs;
@@ -30,6 +31,13 @@ public class ThirdPersonShooterController : MonoBehaviour
         {
             aimVirtualCamera.gameObject.SetActive(false);
             thirdPersonController.SetSensitivity(normalSensitivity);
-        }
+        }   
+            // Find position on the centre of the screen.
+            Vector2 screenCenterPoint = new Vector2(Screen.width / 2f, Screen.height / 2f);
+            // Produce CrossHair RayCast
+            Ray ray = Camera.main.ScreenPointToRay(screenCenterPoint);
+            if (Physics.Raycast(ray, out RaycastHit raycastHit, 999f, aimColliderLayerMask)) {
+                debugTransform.position = raycastHit.point;
+            }
     }
 }
